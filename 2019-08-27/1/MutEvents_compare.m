63b091acfa33c17d8ea4cb7b358465ed41ef6f45
li-xie <lixie943@gmail.com>
2019-09-18 12:34:56 -0700

    mutation restricted to last 10% of each maturation cycle

---
+++ 2019-08-27/1/MutEvents_compare.m
@@ -0,0 +1,12 @@
+clear
+cycle_num = 200;
+mut_rate = 2e-2;
+t_binnum = 340;
+mut_tnum = t_binnum*8/10;
+mut_event_all = OneCycleReplay_mut(cycle_num, mut_rate/2, 0);
+mut_event_res = OneCycleReplay_mut(cycle_num, mut_rate, mut_tnum);
+%%
+histogram(mut_event_all,(0: max(mut_event_all)));
+hold on
+histogram(mut_event_res,(0: max(mut_event_res)));
+hold off
