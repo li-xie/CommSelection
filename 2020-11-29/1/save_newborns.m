0dce0388db55e63198008bcb6fea8c86e93725c3
li-xie <lixie943@gmail.com>
2020-12-03 12:11:03 -0800

    delete saving winner index

--- 2020-11-29/1/save_newborns.m
+++ 2020-11-29/1/save_newborns.m
@@ -7,6 +7,6 @@ nb.newb_L_help = newb_L_help;
 nb.newb_N_help = newb_N_help;
 nb.newb_n_genos = newb_n_genos;
 nb.gen = gen + 1;
-nb.win_inds = win_inds;
+% nb.win_inds = win_inds;
 save(strcat(resultsfolder,'nb',num2str(gen + 1)),'-struct','nb');
 end
